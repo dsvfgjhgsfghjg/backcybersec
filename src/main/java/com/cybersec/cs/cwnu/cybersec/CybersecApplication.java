592e399b6eed42bf3b9beda26e310a446ec3edc0
daxia <orchidxiao@outlook.com>
2025-04-27 19:10:10 +0800

    Creat Project

---
+++ src/main/java/com/cybersec/cs/cwnu/cybersec/CybersecApplication.java
@@ -0,0 +1,13 @@
+package com.cybersec.cs.cwnu.cybersec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybersecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CybersecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybersec/cs/cwnu/cybersec/CybersecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersec.cs.cwnu.cybersec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybersecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
